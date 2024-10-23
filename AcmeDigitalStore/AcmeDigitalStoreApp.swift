42e39db505be64a90b13a337b0ecdffcdc1b5a60
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2024-10-23 11:26:28 -0400

    Initial Commit

---
+++ AcmeDigitalStore/AcmeDigitalStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcmeDigitalStoreApp.swift
+//  AcmeDigitalStore
+//
+//  Created by Arup Sarkar on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AcmeDigitalStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
